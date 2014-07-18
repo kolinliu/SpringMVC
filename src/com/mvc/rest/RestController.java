dbb18a7b0a434fb8e3e25175959b0501b66649f8
kolinliu <932100375@qq.com>
2014-07-18 17:23:49 +0800

    first version

---
+++ src/com/mvc/rest/RestController.java
@@ -0,0 +1,26 @@
+package com.mvc.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class RestController {
+	public RestController() {}
+
+	@RequestMapping(value = "/login/{user}", method = RequestMethod.GET)
+	public ModelAndView myMethod( @PathVariable("user") String user,
+			ModelMap modelMap) {
+		modelMap.put("loginUser", user);
+		return new ModelAndView("login/hello", modelMap);
+	}
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String registerPost(){
+		return "welcome";
+	}
+	
+}
